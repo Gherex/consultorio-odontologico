719ef022a9511a7dacfbca7fb0f081e496833901
Gherex <germilagger@gmail.com>
2025-04-24 04:02:01 -0300

    Añadi plantilla frontend

--- src/main/java/com/gherex/logic/LogicController.java
+++ src/main/java/com/gherex/logic/LogicController.java
@@ -1,30 +1,6 @@
 package com.gherex.logic;
 
-import com.gherex.persistence.PersistenceController;
-
-import java.util.List;
 
 public class LogicController {
 
-    PersistenceController persisControl = new PersistenceController();
-
-    public void crearUsuario(Usuario usuario) {
-        persisControl.crearUsuario(usuario);
-    }
-
-    public List<Usuario> traerUsuarios() {
-        return persisControl.traerUsuarios();
-    }
-
-    public void eliminarUsuario(String idSeleccionado) {
-        persisControl.eliminarUsuario(idSeleccionado);
-    }
-
-    public Usuario traerUsuario(int idSeleccionado) {
-        return persisControl.traerUsuario(idSeleccionado);
-    }
-
-    public void actualizarUsuario(Usuario usuarioEdit) {
-        persisControl.actualizarUsuario(usuarioEdit);
-    }
 }
